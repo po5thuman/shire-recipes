3aa0146dddb4ebef24bb880d271e34b64abd1f92
po5thuman <cardonad@gmail.com>
2026-05-24 15:50:20 +0200

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -2,36 +2,43 @@ const recipeData = [
   {
     name: "Roasted Zucchini w/Garlic Sauce",
     link: "https://www.facebook.com/share/r/1CjtfmMvH1/",
-    tags: ["Zucchini", "Garlic", "Sides"]
+    tags: ["Zucchini", "Garlic", "Sides"],
+    ingredients: ["Zucchini", "Garlic", "Olive oil", "Greek yogurt", "Herbs"]
   },
   {
     name: "Aubergine Lasagna",
     link: "https://www.facebook.com/share/r/16sxgB97QZ/",
-    tags: ["Aubergine", "Pasta", "Main"]
+    tags: ["Aubergine", "Pasta", "Main"],
+    ingredients: ["Aubergine", "Lasagna sheets", "Tomato sauce", "Mozzarella", "Ricotta"]
   },
   {
     name: "Quinoa Salad",
     link: "https://www.facebook.com/reel/1006490817927173/",
-    tags: ["Quinoa", "Salad", "Grains"]
+    tags: ["Quinoa", "Salad", "Grains"],
+    ingredients: ["Quinoa", "Cucumber", "Cherry tomatoes", "Feta cheese", "Lemon dressing"]
   },
   {
     name: "Smashed Potato Salad",
     link: "https://www.facebook.com/share/r/19dUEAifSM/",
-    tags: ["Potato", "Salad"]
+    tags: ["Potato", "Salad"],
+    ingredients: ["Baby potatoes", "Mayonnaise or Yogurt", "Chives", "Mustard"]
   },
   {
     name: "Borani",
     link: "https://www.facebook.com/reel/944165787786671/",
-    tags: ["Yogurt", "Spinach", "Dip"]
+    tags: ["Yogurt", "Spinach", "Dip"],
+    ingredients: ["Spinach", "Strained yogurt (Labneh)", "Garlic", "Walnuts", "Olive oil"]
   },
   {
     name: "Zucchini Pasta",
     link: "https://www.facebook.com/share/r/14DZfpZZKGz/",
-    tags: ["Zucchini", "Pasta"]
+    tags: ["Zucchini", "Pasta"],
+    ingredients: ["Zucchini spaghetti", "Garlic", "Parmesan", "Olive oil", "Chili flakes"]
   },
   {
     name: "Aubergine & Quinoa Bowl",
     link: "https://www.facebook.com/share/r/1A5QNVuCFU/",
-    tags: ["Aubergine", "Quinoa", "Grains", "Main"]
+    tags: ["Aubergine", "Quinoa", "Grains", "Main"],
+    ingredients: ["Aubergine", "Quinoa", "Sun-dried tomatoes", "Mushrooms", "Pesto"]
   }
 ];
