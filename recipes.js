7df0ca823375233239bb17b483b96cb439312b0f
po5thuman <cardonad@gmail.com>
2026-05-24 15:38:38 +0200

    Add recipe data for various dishes

---
+++ recipes.js
@@ -0,0 +1,37 @@
+const recipeData = [
+  {
+    name: "Roasted Zucchini w/Garlic Sauce",
+    link: "https://www.facebook.com/share/r/1CjtfmMvH1/",
+    tags: ["Zucchini", "Garlic", "Sides"]
+  },
+  {
+    name: "Aubergine Lasagna",
+    link: "https://www.facebook.com/share/r/16sxgB97QZ/",
+    tags: ["Aubergine", "Pasta", "Main"]
+  },
+  {
+    name: "Quinoa Salad",
+    link: "https://www.facebook.com/reel/1006490817927173/",
+    tags: ["Quinoa", "Salad", "Grains"]
+  },
+  {
+    name: "Smashed Potato Salad",
+    link: "https://www.facebook.com/share/r/19dUEAifSM/",
+    tags: ["Potato", "Salad"]
+  },
+  {
+    name: "Borani",
+    link: "https://www.facebook.com/reel/944165787786671/",
+    tags: ["Yogurt", "Spinach", "Dip"]
+  },
+  {
+    name: "Zucchini Pasta",
+    link: "https://www.facebook.com/share/r/14DZfpZZKGz/",
+    tags: ["Zucchini", "Pasta"]
+  },
+  {
+    name: "Aubergine & Quinoa Bowl",
+    link: "https://www.facebook.com/share/r/1A5QNVuCFU/",
+    tags: ["Aubergine", "Quinoa", "Grains", "Main"]
+  }
+];
