79cee1a06802dae8fccb436a05cc83bc5d71a672
po5thuman <cardonad@gmail.com>
2026-06-07 00:02:00 +0200

    Fix ingredient and tag capitalization in recipes

--- recipes.js
+++ recipes.js
@@ -3,19 +3,19 @@ const recipeData = [
     name: "Roasted Zucchini",
     link: "https://www.facebook.com/share/r/1CjtfmMvH1/",
     tags: ["Zucchini"],
-    ingredients: ["Zucchini", "Garlic", "Olive oil", "Greek yogurt", "Feta Cheese"],
+    ingredients: ["Zucchini", "Garlic", "Olive Oil", "Greek Yogurt", "Feta Cheese"],
     embedUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1690375315067660%2F&show_text=false&width=267&t=0"
   },
   {
     name: "Aubergine Lasagna",
     link: "https://www.facebook.com/share/r/16sxgB97QZ/",
-    tags: ["Aubergine"],
+    tags: ["Aubergine", "Lasagna"],
     ingredients: ["2 aubergines", "garlic", "olive oil", "salt & pepper", "red onion", "1 tsp smoked paprika", "sundried tomatoes", "2 tbsp tomato purée", "1 tbsp balsamic vinegar", "2 x 400g tins plum tomatoes", "250g cooked puy lentils", "200ml water", "100g walnuts", "10g fresh oregano", "150ml plant-based cream", "8 dried lasagna sheets", "100g plant-based cheese"]
   },
   {
     name: "Quinoa Salad",
     link: "https://www.facebook.com/reel/1006490817927173/",
-    tags: ["Quinoa", "Aubergine"],
+    tags: ["Quinoa", "Aubergine", "Salad"],
     ingredients: ["Aubergine", "Cherry Tomatoes", "Garlic", "Salt & Pepper", "Olive Oil", "Quinoa", "Lentils", "Jarred Pepper", "Sundried Tomatoes", "Cashews", "Lemon Juice"],
     embedUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1006490817927173%2F&show_text=false&width=267&t=0"
   },
