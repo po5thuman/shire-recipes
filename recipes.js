6b74bef6211c7cfc1d60d7dcb5d72fd3b525568a
po5thuman <cardonad@gmail.com>
2026-06-09 10:27:32 +0200

    Remove Turkish Eggs recipe from recipes.js

--- recipes.js
+++ recipes.js
@@ -466,13 +466,6 @@ const recipeData = [
     embedUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F924983697108275%2F&show_text=false&width=267&t=0"
   },
   {
-    name: "Turkish Eggs",
-    link: "https://www.facebook.com/share/r/1LPsVutNRp/",
-    tags: ["Breakfast", "Eggs", "Yogurt"],
-    ingredients: ["4 eggs", "350g yogurt", "garlic", "butter", "Salt & Pepper", "dill"],
-    embedUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1518963022901739%2F&show_text=false&width=267&t=0"
-  },
-  {
     name: "Baked Oats",
     link: "https://www.facebook.com/share/r/1A2iWdVZpw/",
     tags: ["Oats", "Bake"],
